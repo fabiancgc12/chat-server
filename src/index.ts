46fc8f9f1390517c22b83b5c664312523276066e
fabian graterol <fabiancgraterol@gmail.com>
2022-12-04 18:15:21 -0400

    setting up cors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import express from "express";
 import * as http from "node:http";
-import {ServerResponse} from "http";
+import {Server} from "socket.io"
+import cors from "cors"
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const corsOption = {
+    origin: "http://localhost:3000"
+}
 
 const app = express();
+app.use(cors(corsOption))
 const server = http.createServer(app)
+const io = new Server(server,{
+    cors:corsOption
+})
+const port = process.env.PORT || 3001
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+io.on('connection', (socket) => {
+    console.log('a user connected');
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+server.listen(port, () => {
+    console.log(`listening on *:${port}`);
 });
