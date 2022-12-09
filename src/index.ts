5974ec616d2cda96d5c7928badd06d16f3e8432a
fabian graterol <fabiancgraterol@gmail.com>
2022-12-08 21:37:24 -0400

    adding express api for login

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import * as http from "node:http";
 import {Server as SocketServer} from "socket.io"
 import cors from "cors"
 import morgan from "morgan"
+import bodyParser from "body-parser"
 import {MessageModel} from "./utils/MessageModel";
+import {LoginModel} from "./utils/loginModel";
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
@@ -21,13 +23,25 @@ const io = new SocketServer(server,{
 
 app.use(morgan("dev"))
 app.use(cors())
+app.use(bodyParser.json())
 
+app.post("/login",(req,res) => {
+    const {username} = req.body
+    console.log(username)
+    res.send({
+        username
+    })
+    // users.set(newUser)
+})
+
+const users:Map<string,string> = new Map([])
 
 io.on('connection', (socket) => {
     console.log(`user ${socket.id} connected`)
 
     socket.on("disconnect",() => {
         console.log(`user ${socket.id} has disconnected`)
+        users.delete(socket.id)
     })
 
     socket.on("message",(msg:string) => {
